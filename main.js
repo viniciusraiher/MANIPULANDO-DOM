bc3ba6be8b67b5d1e293c36e8029b64a928bd664
viniciusraiher <114488764+viniciusraiher@users.noreply.github.com>
2023-09-11 11:38:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
 const controle = document.querySelector("[data-controle]")
 
-controle.forEach( (elemento) => {
-    elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode)
-    })
-})
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector("[data-contador]")
-
-    if(operacao === "-") {
-        peca.value = parseInt(peca.value) - 1
-    } else {
-        peca.value = parseInt(peca.value) + 1
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
 }
+pecas
+
+(bracos: {-}, blindagens: {-}, nucleos:{-}, pernas: {-}, foguetes{-})
+pecas.bracos
+    {forca: 29, poder: 35, energia: -21, velocidade: -5}
+        "energia": -21,
+        "forca": 29,
+        "poder": 35,
+        "velocidade": -5
+            [[Prototype]]: Objetc
+            pecas.braco.energia
+-21
+pecas["bracos"]
+    {forca: 29, poder: 35, energia: -21, velocidade: -5}
+
+<
